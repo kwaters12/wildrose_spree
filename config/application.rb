c4c16959f14b54a6a205998decf0a081944a58d6
kwaters12 <kellywaters@gmail.com>
2014-07-21 14:48:14 -0700

    Heroku fix 1

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module Store
   class Application < Rails::Application
     
@@ -32,5 +34,6 @@ module Store
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.initialize_on_precompile = false
   end
 end
